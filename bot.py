c53dc62d530c03dd63ee804ca88512d00e7fc384
ktulhoo86 <ktulhoo.pyneng@yandex.ru>
2021-09-16 12:34:37 +0300

    add astrolog func

--- bot.py
+++ bot.py
@@ -1,4 +1,22 @@
+"""
+Домашнее задание №1
+
+Использование библиотек: ephem
+
+* Установите модуль ephem
+* Добавьте в бота команду /planet, которая будет принимать на вход
+  название планеты на английском, например /planet Mars
+* В функции-обработчике команды из update.message.text получите
+  название планеты (подсказка: используйте .split())
+* При помощи условного оператора if и ephem.constellation научите
+  бота отвечать, в каком созвездии сегодня находится планета.
+
+"""
+
+from datetime import datetime, timedelta
+import locale
 import logging
+import ephem
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import settings
 
@@ -38,10 +56,52 @@ def lets_talk(update, context):
         update.message.reply_text(f'сам ты {user_text}')
 
 
+def astrolog(update, context):
+    planet_name = update.message.text
+    print(planet_name)
+    planet_name = planet_name.split()
+    print(planet_name)
+    planet_name = planet_name[1]
+    print(planet_name)
+    planet_name = planet_name.capitalize()
+    print(planet_name)
+    locale.setlocale(locale.LC_ALL, "russian")
+    date_now = datetime.now()
+    delta_1 = timedelta(days=1)
+    tomorrow = date_now + delta_1
+    if 'Меркурий' in planet_name:
+        planet_name = ephem.Mercury(tomorrow)
+        constellation = ephem.constellation(planet_name)
+    elif 'Венера' in planet_name:
+        planet_name = ephem.Venus(tomorrow)
+        constellation = ephem.constellation(planet_name)
+    elif 'Земля' in planet_name:
+        planet_name = ephem.Earth(tomorrow)
+        constellation = ephem.constellation(planet_name)
+    elif 'Марс' in planet_name:
+        planet_name = ephem.Mars(tomorrow)
+        constellation = ephem.constellation(planet_name)
+    elif 'Юпитер' in planet_name:
+        planet_name = ephem.Jupiter(tomorrow)
+        constellation = ephem.constellation(planet_name)
+    elif 'Сатурн' in planet_name:
+        planet_name = ephem.Saturn(tomorrow)
+        constellation = ephem.constellation(planet_name)
+    elif 'Уран' in planet_name:
+        planet_name = ephem.Uranus(tomorrow)
+        constellation = ephem.constellation(planet_name)
+    elif 'Нептун' in planet_name:
+        planet_name = ephem.Neptune(tomorrow)
+        constellation = ephem.constellation(planet_name)
+    print(constellation)
+    update.message.reply_text(constellation)
+
+
 def main():
     mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=PROXY)
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler("planet", astrolog))
     dp.add_handler(MessageHandler(Filters.text, lets_talk))
     logging.info('Bot started')
     mybot.start_polling()
